6a650822bef6b555ce69eb6e7ed149e420697f58
michalbendavid <michalbendavid770@gmail.com>
2022-11-15 22:23:09 +0200

    fixregister function

--- src/App.js
+++ src/App.js
@@ -2,41 +2,27 @@ import './App.css';
 import {useState, useEffect} from "react";
 import {createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "firebase/auth";
 import { auth } from "./firebase.js";
-
 function App() {
   //States for register form and for login form
 const [registerEmail, setRegisterEmail] = useState("");
 const [registerPassword, setRegisterPassword] = useState("");
 const [loginEmail, setLoginEmail] = useState("");
 const [loginPassword, setLoginPassword] = useState("");
-
 // //state for user so it will not throw error when refresh page sets the user to current user when it is logged in
 const [user, setUser] = useState({});
-// onAuthStateChanged(auth , (currentUser) => {
-//   setUser(currentUser);
-// });
-// const handleRegister = (event) => {
-//   event.preventDefault()
-//   register(registerEmail, registerPassword)
-// }
-
-// const handleLogin = (event) => {
-//   event.preventDefault()
-//   login(loginEmail, loginPassword)
-// }
-
 useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    // const unsubscribe =
+     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
-    return unsubscribe;
+    // return unsubscribe;
   }, []);
-
 // functions for login, register and logout
-const register = async (registerEmail, registerPassword) => {
+const register = async () => {
   try{
-    const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-    console.log(user)
+    console.log({auth, registerEmail, registerPassword} );
+    const newUser = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+    console.log(newUser)
     } catch (error) {
     console.log(error.message);
     }
@@ -57,19 +43,15 @@ const register = async (registerEmail, registerPassword) => {
     <div className="App">
       <div>
         <h3> Register User </h3>
-        
-          <input type = "email" placeholder="Email..." onChange= {(event) => {setRegisterEmail(event.target.value)}} value = {registerEmail}/>
-          <input type = "password" placeholder="Password..."  onChange= {(event) => {setRegisterPassword(event.target.value)}} value= {registerPassword}/>
+          <input  placeholder="Email..."  value = {registerEmail} onChange= {(event) => {setRegisterEmail(event.target.value)}}  />
+          <input  placeholder="Password..." value= {registerPassword}  onChange= {(event) => {setRegisterPassword(event.target.value)}}  />
           <button type = "submit" onClick={register}>Creat User</button>
-        
       </div>
       <div> 
         <h3>Log In</h3>
-      
           <input type= "email" placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value)}} value = {loginEmail}/>
           <input type = "password" placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value)}} value = {loginPassword} />
           <button type="submit" onClick={login}>Log In</button> 
-             
       </div> 
       <h4> User Logged In: </h4>
   {/* shows the user name that is loged in right now    */}
--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,16 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY ,
-  authDomain: process.env.AUTO_DOMAIN ,
+  authDomain: process.env.AUTH_DOMAIN ,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET ,
   messagingSenderId: process.env.MESSAGING_SENDERS_ID ,
   appId: process.env.API_ID
 };
+
+console.warn(firebaseConfig);
+setTimeout(() => {console.log(process.env)
+}, 5000);
 
 const app = initializeApp(firebaseConfig);
 
