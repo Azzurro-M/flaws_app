002c1b035cb1946bc0d3ca80f581c10c59142f0e
michalbendavid <michalbendavid770@gmail.com>
2022-11-15 15:20:51 +0200

    register function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import './App.css';
+import {useState} from "react";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import { auth } from "./firebase.js";
+
 
 function App() {
+const [registerEmail, setRegisterEmail] = useState("");
+const [registerPassword, setRegisterPassword] = useState("");
+const [loginEmail, setLoginEmail] = useState("");
+const [loginPassword, setLoginPassword] = useState("");
+
+  const register = async () => {
+    try{
+   const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+   console.log(user)
+    } catch (error) {
+      console.log(error.message);
+    }
+  }; 
+  
+  const login = async () => {
+
+  };
+
+  const logout = async () => {
+
+  };
+
+
   return (
     <div className="App">
       <div>
         <h3> Register User </h3>
-        <input placeholder="Email..." />
-        <input placeholder="Password..." />
-        <button>Creat User</button>
+        <input placeholder="Email..." onChange= {(event) => {setRegisterEmail(event.target.value);}} />
+        <input placeholder="Password..."  onChange= {(event) => {setRegisterPassword(event.target.value);}}/>
+        <button onClick={register}>Creat User</button>
       </div>
       <div>
         <h3>Log In</h3>
-        <input placeholder="Email..." />
-        <input placeholder="Password..." />
+        <input placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value);}} />
+        <input placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value);}} />
         <button>Log In</button>      
       </div> 
       <h4> User Logged In: </h4>
