0c76163bb41176e138c675cd4d7b57b201a151bf
michalbendavid <michalbendavid770@gmail.com>
2022-11-15 13:51:45 +0200

    set up firebase.js file

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ function App() {
         <input placeholder="Email..." />
         <input placeholder="Password..." />
         <button>Creat User</button>
+      </div>
+      <div>
+        <h3>Log In</h3>
+        <input placeholder="Email..." />
+        <input placeholder="Password..." />
+        <button>Log In</button>      
       </div> 
-      
+      <h4> User Logged In: </h4>
+      <button> Sign Out </button>
     </div>
+    
   );
 }
 
