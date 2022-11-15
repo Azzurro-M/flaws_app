5a56e2c45a868c7d3a212f3d2452c8f5145592ee
michalbendavid <michalbendavid770@gmail.com>
2022-11-15 18:41:44 +0200

    try debugging loop that make the forms get stuck

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import {createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "firebase/auth";
 import { auth } from "./firebase.js";
-
 
 function App() {
   //States for register form and for login form
@@ -16,17 +15,32 @@ const [user, setUser] = useState({})
 onAuthStateChanged(auth , (currentUser) => {
   setUser(currentUser);
 });
+const handleRegister = (event) => {
+  event.preventDefault()
+  register(registerEmail, registerPassword)
+}
+
+const handleLogin = (event) => {
+  event.preventDefault()
+  login(loginEmail, loginPassword)
+}
+
+useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return unsubscribe;
+  }, []);
 
 // functions for login, register and logout
-  const register = async () => {
-    try{
-      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      console.log(user)
+const register = async (registerEmail, registerPassword) => {
+  try{
+    const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+    console.log(user)
     } catch (error) {
-      console.log(error.message);
+    console.log(error.message);
     }
   }; 
-  
   const login = async () => {
      try{
       const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
@@ -34,34 +48,34 @@ onAuthStateChanged(auth , (currentUser) => {
     } catch (error) {
       console.log(error.message);
     }
-
   };
-
   const logout = async () => {
     await signOut(auth);
   };
-
 
   return (
     <div className="App">
       <div>
         <h3> Register User </h3>
-        <input placeholder="Email..." onChange= {(event) => {setRegisterEmail(event.target.value);}} />
-        <input placeholder="Password..."  onChange= {(event) => {setRegisterPassword(event.target.value);}}/>
-        <button onClick={register}>Creat User</button>
+        <form onClick = {handleRegister}>
+          <input type = "email" placeholder="Email..." onChange= {(event) => {setRegisterEmail(event.target.value)}} value = {registerEmail}/>
+          <input type = "password" placeholder="Password..."  onChange= {(event) => {setRegisterPassword(event.target.value)}} value= {registerPassword}/>
+          <button type = "submit" onClick={register}>Creat User</button>
+        </form>
       </div>
-      <div>
+      <div> 
         <h3>Log In</h3>
-        <input placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value);}} />
-        <input placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value);}} />
-        <button onClick={login}>Log In</button>      
+        <form onClick={handleLogin}>
+          <input type= "email" placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value)}} value = {loginEmail}/>
+          <input type = "password" placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value)}} value = {loginPassword} />
+          <button type="submit" onClick={login}>Log In</button> 
+        </form>     
       </div> 
       <h4> User Logged In: </h4>
   {/* shows the user name that is loged in right now    */}
       {user?.email}
       <button onClick={logout}> Sign Out </button>
     </div>
-    
   );
 }
 
