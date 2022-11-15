782215a3752aa5e356158fac476feffb51afae98
michalbendavid <michalbendavid770@gmail.com>
2022-11-15 19:35:49 +0200

    debug

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ const [registerPassword, setRegisterPassword] = useState("");
 const [loginEmail, setLoginEmail] = useState("");
 const [loginPassword, setLoginPassword] = useState("");
 
-//state for user so it will not throw error when refresh page sets the user to current user when it is logged in
-const [user, setUser] = useState({})
-onAuthStateChanged(auth , (currentUser) => {
-  setUser(currentUser);
-});
-const handleRegister = (event) => {
-  event.preventDefault()
-  register(registerEmail, registerPassword)
-}
+// //state for user so it will not throw error when refresh page sets the user to current user when it is logged in
+const [user, setUser] = useState({});
+// onAuthStateChanged(auth , (currentUser) => {
+//   setUser(currentUser);
+// });
+// const handleRegister = (event) => {
+//   event.preventDefault()
+//   register(registerEmail, registerPassword)
+// }
 
-const handleLogin = (event) => {
-  event.preventDefault()
-  login(loginEmail, loginPassword)
-}
+// const handleLogin = (event) => {
+//   event.preventDefault()
+//   login(loginEmail, loginPassword)
+// }
 
 useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -57,19 +57,19 @@ const register = async (registerEmail, registerPassword) => {
     <div className="App">
       <div>
         <h3> Register User </h3>
-        <form onClick = {handleRegister}>
+        
           <input type = "email" placeholder="Email..." onChange= {(event) => {setRegisterEmail(event.target.value)}} value = {registerEmail}/>
           <input type = "password" placeholder="Password..."  onChange= {(event) => {setRegisterPassword(event.target.value)}} value= {registerPassword}/>
           <button type = "submit" onClick={register}>Creat User</button>
-        </form>
+        
       </div>
       <div> 
         <h3>Log In</h3>
-        <form onClick={handleLogin}>
+      
           <input type= "email" placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value)}} value = {loginEmail}/>
           <input type = "password" placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value)}} value = {loginPassword} />
           <button type="submit" onClick={login}>Log In</button> 
-        </form>     
+             
       </div> 
       <h4> User Logged In: </h4>
   {/* shows the user name that is loged in right now    */}
