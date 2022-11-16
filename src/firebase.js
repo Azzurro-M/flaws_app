3f82b7ab97d31169c1b8bb7769d8ec11666b541c
michalbendavid <michalbendavid770@gmail.com>
2022-11-16 15:33:32 +0200

    fix bug on .env and firebase variable names

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY ,
-  authDomain: process.env.AUTH_DOMAIN ,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET ,
-  messagingSenderId: process.env.MESSAGING_SENDERS_ID ,
-  appId: process.env.API_ID
+  apiKey: process.env.REACT_APP_API_KEY , 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN , 
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERS_ID ,
+  appId: process.env.REACT_APP_API_ID
 };
 
 console.warn(firebaseConfig);
