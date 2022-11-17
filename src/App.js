167a77f865db5e79e7d5f88528f017ccc1bbb37a
Noam Belleli <noamhoter@gmail.com>
2022-11-17 15:01:17 +0200

    addded sytilng and rendered to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import { useAuth } from './context/auth.Context';
+import {useState, useEffect} from "react";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "firebase/auth";
+import { auth } from "./firebase.js";
+import Navbar from './components/Navbar';
 
 function App() {
   const {user} = useAuth();
 
-  if (user) return <div>{user.email}</div>;
-  return <div className='App'>
-      Flaws - Home Page
-      {/* <button onClick={logout}> Sign Out </button>  */}
+  return (
+    
+    <div className="App">
+      <Navbar />
+      <div>
+        <h3> Register User </h3>
+          <input  placeholder="Email..."  value = {registerEmail} onChange= {(event) => {setRegisterEmail(event.target.value)}}  />
+          <input  placeholder="Password..." value= {registerPassword}  onChange= {(event) => {setRegisterPassword(event.target.value)}}  />
+          <button type = "submit" onClick={register}>Creat User</button>
+      </div>
+      <div> 
+        <h3>Log In</h3>
+          <input type= "email" placeholder="Email..." onChange= {(event) => {setLoginEmail(event.target.value)}} value = {loginEmail}/>
+          <input type = "password" placeholder="Password..." onChange= {(event) => {setLoginPassword(event.target.value)}} value = {loginPassword} />
+          <button type="submit" onClick={login}>Log In</button> 
+      </div> 
+      <h4> User Logged In: </h4>
+  {/* shows the user name that is loged in right now    */}
+      {user?.email}
+      <button onClick={logout}> Sign Out </button>
     </div>
 
 }
