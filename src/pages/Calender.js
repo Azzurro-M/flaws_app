5cfaacf279929855b0457cac6ae4380bde398778
Noam Belleli <noamhoter@gmail.com>
2022-11-29 15:31:09 +0200

    addedNavbar and logout to calender

--- src/pages/Calender.js
+++ src/pages/Calender.js
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import Navbar from "../components/Navbar";
+import { useAuth } from "../context/auth.Context";
+import App from "../App";
 
 function CalendarPage() {
   const [date, setDate] = useState(new Date());
+  const { user } = useAuth();
 
+  if (user) {
+    return (
+      <>
+        <Navbar />
+        <div className="app">
+          <h1 className="text-center">React Calendar</h1>
+          <div className="calendar-container">
+            <Calendar onChange={setDate} value={date} />
+          </div>
+          <p className="text-center">
+            <span className="bold">Selected Date:</span> {date.toDateString()}
+          </p>
+        </div>
+      </>
+    );
+  }
   return (
-    <div className="app">
-      <h1 className="text-center">React Calendar</h1>
-      <div className="calendar-container">
-        <Calendar onChange={setDate} value={date} />
-      </div>
-      <p className="text-center">
-        <span className="bold">Selected Date:</span> {date.toDateString()}
-      </p>
-    </div>
+    <>
+      <App />
+    </>
   );
 }
 
