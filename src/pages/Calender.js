64e78b6671a23433c7111c16604d5bab63571fa4
Noam Belleli <noamhoter@gmail.com>
2022-11-29 15:19:30 +0200

    added calender from marianna branch

--- src/pages/Calender.js
+++ src/pages/Calender.js
@@ -1,37 +1,20 @@
-// import React from "react";
-// import Navbar from "../components/Navbar";
-// import Calendar from "react-calendar";
-// import { useState } from "react";
+import { useState } from "react";
+import Calendar from "react-calendar";
 
-// function PeriodCalendar() {
-//   const [date, setDate] = useState(new Date());
-//   return (
-//     <div className="calendar">
-//       <Navbar />
-//       <h1 className="text-center">React Calendar</h1>
-//       <div className="calendar-container">
-//         <Calendar onChange={setDate} value={date} />
-//       </div>
-//       <p className="text-center">
-//         <span className="bold">Selected Date:</span> {date.toDateString()}
-//       </p>
-//     </div>
-//   );
-// }
+function CalendarPage() {
+  const [date, setDate] = useState(new Date());
 
-// export default PeriodCalendar;
+  return (
+    <div className="app">
+      <h1 className="text-center">React Calendar</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className="text-center">
+        <span className="bold">Selected Date:</span> {date.toDateString()}
+      </p>
+    </div>
+  );
+}
 
-import React from "react";
-import Navbar from "../components/Navbar";
-
-const Calender = () => {
-    return (
-        <div>
-          <Navbar />
-          <h1>Calender</h1>
-        </div>
-      );
-    };
-
-
-export default Calender;
+export default CalendarPage;
